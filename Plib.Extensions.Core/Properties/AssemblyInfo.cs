723c96372a4ab37cdb36c694792a602e65406266
Patrik Eriksson <jippegbg@hotmail.com>
2018-06-29 18:45:09 +0200

    Rename Plib to PLib

--- Plib.Extensions.Core/Properties/AssemblyInfo.cs
+++ Plib.Extensions.Core/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Plib.Extensions.Core")]
+[assembly: AssemblyTitle("PLib.Extensions.Core")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Plib.Extensions.Core")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PLib.Extensions.Core")]
+[assembly: AssemblyCopyright("Copyright © Patrik Eriksson 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
